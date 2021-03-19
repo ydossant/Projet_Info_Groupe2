4cf86cc8bc7bf93662dba98770ce62f007af0126
Londuzboub <Londuzboub@MSI.home>
2021-03-19 17:58:49 +0100

    heros

--- src/Heros/Heros.java
+++ src/Heros/Heros.java
@@ -1,5 +1,59 @@
 package Heros;
 
-public class Heros {
 
+public class Heros {
+	
+	private int PV;
+	private final int PV_Depart;
+	private boolean Objets = false;
+	
+	public Heros() 
+	{
+		
+	setPV(PV);
+	this.PV_Depart= this.PV;
+	
+	}
+
+	public int getPV() {
+		
+		return this.PV;
+	}
+
+	public void setPV(int pV) {
+		
+		if ( PV <= 0 )
+	    {
+	        System.out.println("Le heros est mort.");
+	    }
+	    else
+	    {
+	    	if ( PV <= this.PV_Depart )
+	    		this.PV = PV;
+	    	
+	    	else 
+	    		this.PV = this.PV_Depart;
+	    }
+		
+	}
+
+	public boolean getObjets() {
+		
+		return this.Objets;
+	}
+
+	public void setObjets(boolean objets) {
+		
+		Objets = objets;
+	}
+
+	public int getPV_Depart() {
+		
+		return PV_Depart;
+	}
+	
+	
+	
+	
+	
 }
