c4df07b8d99e84645211c34a72c5b48c8d8d6dc6
Londuzboub <Londuzboub@MSI.home>
2021-03-19 17:26:50 +0100

    plateau

--- src/Plateau/Plateau.java
+++ src/Plateau/Plateau.java
@@ -2,4 +2,32 @@ package Plateau;
 
 public class Plateau {
 
+	private int nbrelign ;
+    private int nbrecol;
+    private char [][] grille;
+ 
+    public Plateau(int n,int p ){
+         nbrelign = n;
+         nbrecol = p;
+         grille = new char[nbrelign][nbrecol];
+ 
+         for(int i=0; i< nbrelign; i++){
+            for(int j =0; j< nbrecol; j++){
+             grille[i][j] = '*';
+           }
+        }
+    }
+ 
+    public void afficher(){
+       System.out.println();
+       for(int i=0; i< nbrelign; i++){
+            for(int j  =0; j< nbrecol; j++){
+ 
+             System.out.print(" | " + grille[i][j]);
+           }
+            System.out.println(" | " );
+       }
+       System.out.println();
+   }
+ 
 }
--- src/Plateau/Type_case.java
+++ src/Plateau/Type_case.java
@@ -2,4 +2,5 @@ package Plateau;
 
 public enum Type_case {
 
+	mur, vide, piege, potion, objectif;
 }
