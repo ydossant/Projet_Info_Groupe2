a7de8186828a847c18d4337f275016b9488e6750
cleme <cleme@LAPTOP-HE0OVF8H.home>
2021-03-25 14:01:31 +0100

    test

---
+++ src/main/TestMain.java
@@ -0,0 +1,15 @@
+package main;
+
+import Heros.*;
+import Objets.*;
+import Outils.*;
+import Plateau.*;
+import main.Main;
+
+public class TestMain {
+	public static void main(String[]args) {
+		Heros he = new Heros();
+		System.out.println(he);
+	}
+
+}
