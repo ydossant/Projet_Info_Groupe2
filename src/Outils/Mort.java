e3ac071cc9f34e7955c025c0a0cd5c8f3ee169c0
cleme <cleme@LAPTOP-HE0OVF8H.home>
2021-03-25 13:53:20 +0100

    mort

--- src/Outils/Mort.java
+++ src/Outils/Mort.java
@@ -2,19 +2,11 @@ package Outils;
 
 public class Mort extends Exception{
 
-    /**
-     * C'est l'exception par défaut qui n'envoie pas de message
-     * @see vivants.Heros
-     */
     public Mort ()
     {
         super();
     }
     
-    /**
-     * C'est l'exception qui envoie un message pour la mort du héros
-     * @param Message Le message envoie pour la mort du héros
-     */
     public Mort ( String Message )
     {
         super(Message);
