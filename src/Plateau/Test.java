9c743639bda8ffbf2f364992723202f70f48fed7
cleme <cleme@LAPTOP-HE0OVF8H.home>
2021-03-25 15:53:41 +0100

    test

---
+++ src/Plateau/Test.java
@@ -0,0 +1,11 @@
+package Plateau;
+
+import Plateau.Plateau2;
+
+public class Test {
+	public static void main(String args[]) {
+		Plateau2 plateau = new Plateau2(5,5);
+		plateau.afficher();
+	}
+
+}
