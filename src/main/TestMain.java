73e75efa9631a6d1e1d77c3f4e62a3fb216c9def
Londuzboub <Londuzboub@MSI.home>
2021-03-25 15:35:40 +0100

    testmain

--- src/main/TestMain.java
+++ src/main/TestMain.java
@@ -4,12 +4,11 @@ import Heros.*;
 import Objets.*;
 import Outils.*;
 import Plateau.*;
-import main.*;
 
 public class TestMain {
 	public static void main(String[]args) {
 		Heros he = new Heros();
 		System.out.println(he); 
 	}
-
+	
 }
