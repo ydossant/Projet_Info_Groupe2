70e213baab6ef1364d6a11764e3f8e43781d5b66
cleme <cleme@LAPTOP-HE0OVF8H.home>
2021-03-26 18:02:45 +0100

    UYTR

--- Groupe2projet/src/Plateau/Test.java
+++ Groupe2projet/src/Plateau/Test.java
@@ -2,7 +2,7 @@ package Plateau;
 
 public class Test {
 	public static void main(String args[]) {
-		Plateau plateau = new Plateau(5,7);
+		Plateau plateau = new Plateau(6,7);
 		plateau.afficher();
 	}
 
