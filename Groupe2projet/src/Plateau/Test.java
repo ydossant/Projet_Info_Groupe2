1820d00abb8214ee744edb837da2e5d58a563ef7
remi <remi@remito>
2021-03-26 17:23:44 +0000

    okok

--- Groupe2projet/src/Plateau/Test.java
+++ Groupe2projet/src/Plateau/Test.java
@@ -2,7 +2,7 @@ package Plateau;
 
 public class Test {
 	public static void main(String args[]) {
-		Plateau plateau = new Plateau(7,7);
+		Plateau plateau = new Plateau(8,7);
 		plateau.afficher();
 		int a=1;
 	}
