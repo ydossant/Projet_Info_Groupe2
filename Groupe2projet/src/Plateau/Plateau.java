2682b499cb18f6ea2f495268d99700b87d11db2d
cleme <cleme@LAPTOP-HE0OVF8H.home>
2021-03-28 19:32:03 +0200

    hg

--- Groupe2projet/src/Plateau/Plateau.java
+++ Groupe2projet/src/Plateau/Plateau.java
@@ -1,22 +1,27 @@
 package Plateau;
 
+
 public class Plateau {
 	private static Plateau plat;
     private int nbrelign ;
     private int nbrecol;
-    private char [][] grille;
+    private Piece [][] grille;
+    private Piece pie;
+    Orientation coordHeros = null;
  
     public Plateau(int n,int p ){
-         nbrelign = n;
-         nbrecol = p;
-         grille = new char[nbrelign][nbrecol];
+         nbrelign = this.getNbrelign();
+         nbrecol = this.getNbrecol();
  
          for(int i=0; i< nbrelign; i++){
             for(int j =0; j< nbrecol; j++){
-             grille[i][j] = '?';
+            	Piece pie = new Piece( new Orientation(i, j) );
+				this.set_UneCase(i, j, pie);
            }
         }
     }
+    
+    
  
     public int getNbrelign() {
 		return nbrelign;
@@ -34,12 +39,75 @@ public class Plateau {
 		this.nbrecol = nbrecol;
 	}
 
-	public char[][] getGrille() {
+	public Piece[][] getGrille() {
 		return grille;
 	}
 
-	public void setGrille(char[][] grille) {
+	public void setGrille(Piece[][] grille) {
 		this.grille = grille;
+	}
+	
+	protected Piece get_UneCase(int i, int j)
+	{
+		return this.grille[i][j];
+	}
+	
+	protected final void set_UneCase(int i, int j, Piece pie)
+	{
+		this.grille[i][j] = pie;
+	}
+	
+	protected Orientation get_CoordHeros()
+	{
+		return this.coordHeros;
+	}
+	
+	public String toString()
+	{
+		StringBuffer bf = new StringBuffer("\n");
+		
+		for(int i = 0; i < this.nbrelign; i ++)
+		{
+			bf.append( this.afficheUneLigne(i) );
+		}
+		
+		bf.append("\n");
+		
+		return bf.toString();
+	}
+	
+	protected String afficheUneLigne(int lig)
+	{
+		StringBuffer bf = new StringBuffer("");
+		int nbL = this.getNbrelign();
+		int nbC = this.getNbrecol();
+		
+		bf.append( TravailleChaine.avant_ElementPiece(nbC, 5) );
+		
+		/* 2) On affiche le contenu de chaque pièce pour une ligne */
+		for(int j = 0; j < nbC; j ++)
+		{
+			Piece pi = this.get_UneCase(lig, j);
+
+			String heros = new String();
+
+			Orientation or = pi.getCoord();
+			/* Si le héros se trouve dans la pièce on l'affiche aussi */
+			if( or.equals(this.get_CoordHeros()) )
+			{
+				heros = new String("H");
+			}		
+			StringBuffer ssSB = new StringBuffer(heros);
+			/* On rajoute l'élément qu'il y a dans la pièce */
+			ssSB.append( pi.attribuer_Lettre() );
+			
+			bf.append( TravailleChaine.rajouter_Blanc(5, ssSB.toString(), " ") );	
+		}
+		bf.append("|\n");
+		
+		bf.append( TravailleChaine.apres_ElementPiece(nbC, 5) );
+		
+		return bf.toString();
 	}
 
 	public void afficher(){
@@ -47,7 +115,7 @@ public class Plateau {
        for(int i=0; i< nbrelign; i++){
             for(int j  =0; j< nbrecol; j++){
  
-             System.out.print(" " + grille[i][j]);
+             System.out.print(" " + pie.attribuer_Lettre());
            }
             System.out.println(" " );
        }
--- Groupe2projet/src/Plateau/Test.java
+++ Groupe2projet/src/Plateau/Test.java
@@ -4,11 +4,11 @@ public class Test {
 	public static void main(String args[]) {
 		Plateau plateau = new Plateau(4,5);
 		plateau.afficher();
-		Orientation or = new Orientation(4,5);
-		System.out.println(or);
-		Piece pi = new Piece(or);
-		System.out.println(pi);
-		System.out.println(pi.attribuer_Lettre());
+		//Orientation or = new Orientation(4,5);
+		//System.out.println(or);
+		//Piece pi = new Piece(or);
+		//System.out.println(pi);
+		//System.out.println(pi.attribuer_Lettre());
 	}
 
 }
